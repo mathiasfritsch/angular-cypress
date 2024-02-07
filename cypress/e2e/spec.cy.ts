9f015c4cf5b1d6c7265a1052bc738bd36aae8a7a
Mathias Fritsch <63262184+mathiasfritsch@users.noreply.github.com>
2024-02-07 20:57:00 +0100

    Update spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,18 +1,13 @@
 describe('Products Test', () => {
-  const currentTime = new Date().toLocaleTimeString().replace(/:/g, '_');
-  const testName = `GetProducts at ${currentTime}`;
-
   beforeEach(() => {
     cy.intercept('GET', '/api/products', { fixture: 'products.json' }).as(
       'getProducts'
     );
   });
 
-  it(testName, () => {
+  it('Table row has product', () => {
     cy.visit('/');
-
     cy.get('table tr').should('have.length', 3);
-
     cy.contains('product 2');
   });
 });
